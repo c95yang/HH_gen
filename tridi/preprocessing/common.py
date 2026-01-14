b67941a9467977684008f581ff19b19d1c5ddc06
selin5 <111873767+selin5@users.noreply.github.com>
2026-01-14 22:16:26 +0100

    second_sbj_gender added

--- tridi/preprocessing/common.py
+++ tridi/preprocessing/common.py
@@ -161,10 +161,11 @@ def get_sequences_list(dataset, input_path, subjects=None, objects=None):
     return all_seqs
 
 
-def add_meatada_to_hdf5(seq_group, sbj, T, gender=""):
+def add_meatada_to_hdf5(seq_group, sbj, T, sbj_gender="", second_sbj_gender=""):
     seq_group.attrs['sbj'] = sbj
     seq_group.attrs['T'] = T
-    seq_group.attrs['gender'] = gender
+    seq_group.attrs['sbj_gender'] = sbj_gender
+    seq_group.attrs['second_sbj_gender'] = second_sbj_gender
 
 def add_sequence_datasets_to_hdf5(seq_group, sample, T):
     # object mesh
